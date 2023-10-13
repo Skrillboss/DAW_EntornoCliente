ac892473e6dc3a90b681107991cdb6a8fd9c06da
Luis Heredia <heredialuis20@gmail.com>
2023-10-13 17:45:44 +0200

    falta arreglar errores con el promt y agragar css

---
+++ piloto.js
@@ -0,0 +1,16 @@
+class Piloto{
+
+
+    constructor(nombre, numeroKart, tiempoCarrera){
+
+        this.nombre = nombre;
+        this.numeroKart = numeroKart;
+        this.tiempoCarrera = tiempoCarrera;
+
+
+    }
+
+
+
+
+}
